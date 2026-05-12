3393f97ae9daba4406b51bdc63b1a3b7ee8970a6
frvnrbf9vf-crypto <frvnrbf9vf@privaterelay.appleid.com>
2026-05-12 12:41:38 +0100

    Added GPIO configuration header

---
+++ src/include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DHT_PIN 4
+#define LED_PIN 2
+#define BUZZER_PIN 18
+#define BUTTON_PIN 15
+#define ADC_PIN 34
+
+#define TEMP_THRESHOLD 30.0
+
+#endif
